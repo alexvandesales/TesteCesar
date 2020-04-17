10361d87ac594d13ec04dcb27045abee2cb6ef00
alexvandesales <alexvandesales@users.noreply.github.com>
2020-04-16 21:30:26 -0400

    Add files via upload

---
+++ questao3.py
@@ -0,0 +1,55 @@
+def isEditDistanceOne(first, second):
+    firstLength = len(first)
+    secondLength = len(second)
+
+    if abs(firstLength - secondLength) > 1:
+        return False
+
+    count = 0
+    i = 0
+    j = 0
+
+    while i < firstLength and j < secondLength:
+        if first[i] != second[j]:
+            if count == 1:
+                return False
+
+            if firstLength > secondLength:
+                i = i + 1
+
+            elif firstLength < secondLength:
+                j = j + 1
+            else:
+                i = i + 1
+                j = j + 1
+            count += 1
+        else:
+            i = i + 1
+            j = j + 1
+
+    if i < firstLength or j < secondLength:
+        count = count + 1
+
+    return count == 1
+def main():
+    if isEditDistanceOne("pale", "ple"):
+        print("Yes")
+    else:
+        print("No")
+
+    if isEditDistanceOne("pales", "pale"):
+        print("Yes")
+    else:
+        print("No")
+
+    if isEditDistanceOne("pale", "bale"):
+        print("Yes")
+    else:
+        print("No")
+
+    if isEditDistanceOne("pale", "bake"):
+        print("Yes")
+    else:
+        print("No")
+
+main()
